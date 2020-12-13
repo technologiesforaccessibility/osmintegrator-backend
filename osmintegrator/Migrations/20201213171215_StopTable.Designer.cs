142cc38f12d30ffab8129f40b2ab8f6140b6a47b
Marcin Przybyłowicz <martin.przybylowicz@gmail.com>
2020-12-13 18:17:50 +0100

    Added Stops table to migrations.

--- osmintegrator/Migrations/20201213171215_StopTable.Designer.cs
+++ osmintegrator/Migrations/20201213171215_StopTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using osmintegrator.Database;
@@ -9,9 +10,10 @@ using osmintegrator.Database;
 namespace osmintegrator.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201213171215_StopTable")]
+    partial class StopTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
