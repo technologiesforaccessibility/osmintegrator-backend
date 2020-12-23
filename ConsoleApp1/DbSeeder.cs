c7d13e6277eb40d00a6f91d54dca46acab7a95d0
Marcin Przybyłowicz <martin.przybylowicz@gmail.com>
2020-12-23 08:12:32 +0100

    Added functionality as follow: Adding new records to Stops table from consloe application.

--- ConsoleApp1/DbSeeder.cs
+++ ConsoleApp1/DbSeeder.cs
@@ -5,28 +5,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
 
 namespace ConsoleApp1
 {
-    public class DbSeeder
+    public class DbSeeder : BackgroundService
     {
         private readonly ApplicationDbContext _dbContext;
 
         public DbSeeder(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
-            Seed();
         }
 
-        public void Seed()
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _dbContext.Stops.AddRange(
+            await _dbContext.Stops.AddRangeAsync(
                 new List<Stop>
                 {
-                new Stop { StopId=3000, TypeId = 1, StopName = "Katowice, Kolista 2", Lat=59.345f, Lon=18.4353f},
-                new Stop { StopId=3001, TypeId = 1, StopName = "Katowice, Lipowa 3", Lat=59.645f, Lon=18.8353f},
+                new Stop { StopId=9006, TypeId = 1, StopName = "Katowice, Kolista 2", Lat=59.345f, Lon=18.4353f},
+                new Stop { StopId=9007, TypeId = 1, StopName = "Katowice, Lipowa 3", Lat=59.645f, Lon=18.8353f},
                 }
             );
+            await _dbContext.SaveChangesAsync();
+            Console.WriteLine("New records were added to Stops table.");
+            Console.ReadLine();
         }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,43 +6,35 @@ using osmintegrator.Database.DataInitialization;
 using osmintegrator.Models;
 using System;
 using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static private ServiceProvider serviceProvider;
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            ServiceCollection services = new ServiceCollection();
-            ConfigureServices(services);
-            serviceProvider = services.BuildServiceProvider();
+            using IHost host = CreateHostBuilder(args).Build();
 
-            OnStartup();
+            // Application code should start here.
 
-            Console.ReadLine();
-
-        }
-        static private void ConfigureServices(ServiceCollection services)
-        {
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //{
-            //    options.UseNpgsql("User ID=osm_integrator;Password=super_compolicated_password_12345;Host=localhost;Port=5433;Database=osm_integrator;Pooling=true;");
-            //});
-            services.AddDbContext<ApplicationDbContext>();
-            services.AddSingleton<DbSeeder>();
-        }
-        static void OnStartup()
-        {
-            var mainWindow = serviceProvider.GetService<DbSeeder>();
-            mainWindow.Seed();
+            await host.RunAsync();
         }
 
-        //public static IConfiguration GetConfiguration() =>
-        //    new ConfigurationBuilder()
-        //        .SetBasePath(Directory.GetCurrentDirectory())
-        //        .AddJsonFile("appsettings.json", true, true)
-        //        .Build();
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(
+                    (hostingContext, configuration) => {                        
+                        IConfigurationRoot configurationRoot = configuration.Build();
+                        }
+                    )
+                .ConfigureServices(
+                    (_, services) => {
+                        services
+                            .AddDbContext<ApplicationDbContext>()
+                            .AddHostedService<DbSeeder>();
+                        }
+                    );
     }
 }
