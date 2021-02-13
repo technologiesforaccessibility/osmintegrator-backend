43a2cdbcba13a943128fc09fe61c70025d60663d
Lukasz Taraszka <luktar@gmail.com>
2021-02-13 17:26:48 +0100

    There is possibility to download all stops from tile by tile id.

--- OsmIntegrator/Controllers/TileController.cs
+++ OsmIntegrator/Controllers/TileController.cs
@@ -37,9 +37,41 @@ namespace OsmIntegrator.Controllers
                 var result = await _dbContext.Tiles.Where(x => x.GtfsStopsCount > 0).ToListAsync();
                 return Ok(result);
 
-            } catch(Exception ex)
+            }
+            catch (Exception ex)
             {
-                _logger.LogWarning(ex, "Unknown error while performing ");
+                _logger.LogWarning(ex, $"Unknown error while performing {nameof(Get)} method.");
+                return BadRequest(new UnknownError() { Description = ex.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            try
+            {
+                Guid guidId = Guid.Parse(id);
+                var result = await _dbContext.Tiles.SingleOrDefaultAsync(x => x.Id == guidId);
+
+                if (result == null)
+                {
+                    Error error = new Error();
+                    error.Description = $"Unable to find tile with id {id}.";
+                    error.Message = "General error";
+                    return BadRequest(error);
+                }
+
+                var stops = await _dbContext.Stops.Where(x =>
+                    x.Lon > result.OverlapMinLon && x.Lon <= result.OverlapMaxLon &&
+                    x.Lat > result.OverlapMinLat && x.Lon <= result.OverlapMaxLat).ToListAsync();
+
+                result.Stops = stops;
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, $"Unknown error while performing {nameof(Get)} method with parameter {id}.");
                 return BadRequest(new UnknownError() { Description = ex.Message });
             }
         }
