f5cf2f975279b08aa8f9105a45bde8d8e1b707a7
petrus <piotr.dankowski@revolve.pro>
2021-07-13 13:23:47 +0200

    missing migrations

---
+++ OsmIntegrator/Migrations/20210713112250_AddChangesetAndVersionToStop.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace osmintegrator.Migrations
+{
+    public partial class AddChangesetAndVersionToStop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Changeset",
+                table: "Stops",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "Stops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Changeset",
+                table: "Stops");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Stops");
+        }
+    }
+}
