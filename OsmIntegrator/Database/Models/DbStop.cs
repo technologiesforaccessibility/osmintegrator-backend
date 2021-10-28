7c5f7a0e375b7af6a3cedb7e8a7b841b70da3401
Jakub Gańczorz <jakub.ganczorz@revolve.pro>
2021-10-28 14:49:28 +0200

    WIP

--- OsmIntegrator/Database/Models/DbStop.cs
+++ OsmIntegrator/Database/Models/DbStop.cs
@@ -5,49 +5,50 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OsmIntegrator.Database.Models
 {
-    [Table("Stops")]
-    public class DbStop
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Key]
-        public Guid Id { get; set; }
+  [Table("Stops")]
+  public class DbStop
+  {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public Guid Id { get; set; }
 
-        [Required]
-        public long StopId { get; set; }
+    [Required]
+    public long StopId { get; set; }
 
-        public string Name { get; set; }
+    public string Name { get; set; }
 
-        [Required]
-        public double Lat { get; set; }
+    [Required]
+    public double Lat { get; set; }
 
-        [Required]
-        public double Lon { get; set; }
+    [Required]
+    public double Lon { get; set; }
 
-        public string Number { get; set; }
+    public string Number { get; set; }
 
-        [Column(TypeName = "jsonb")]
-        public List<Tag> Tags { get; set; }
+    [Column(TypeName = "jsonb")]
+    public List<Tag> Tags { get; set; }
 
-        [Required]
-        public StopType StopType { get; set; }
+    [Required]
+    public StopType StopType { get; set; }
 
-        [Required]
-        public ProviderType ProviderType { get; set; }
+    [Required]
+    public ProviderType ProviderType { get; set; }
 
-        public Guid TileId { get; set; }
+    public Guid TileId { get; set; }
 
-        public DbTile Tile { get; set; }
+    public DbTile Tile { get; set; }
 
-        public bool OutsideSelectedTile { get; set; } = false;
+    public bool OutsideSelectedTile { get; set; } = false;
 
-        public List<DbConnections> GtfsConnections { get; set; }
+    public List<DbConnections> GtfsConnections { get; set; }
 
-        public List<DbConnections> OsmConnections { get; set; }
+    public List<DbConnections> OsmConnections { get; set; }
 
-        public long Ref { get; set; }
+    public long Ref { get; set; }
 
-        public int Version { get; set; }
+    public int Version { get; set; }
 
-        public string Changeset { get; set; }
-    }
+    public string Changeset { get; set; }
+    public bool IsDeleted { get; set; }
+  }
 }
