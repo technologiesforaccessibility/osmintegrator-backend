a209662d2ed4287fc5f63033d53db977cdb2715f
Michał Drabik <micdra7@gmail.com>
2022-03-17 15:35:05 +0100

    add tests for gtfs stop revert

---
+++ OsmIntegrator.Tests/Tests/GtfsImports/RevertStopTest.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OsmIntegrator.ApiModels.Reports;
+using OsmIntegrator.Database.Models;
+using OsmIntegrator.Database.Models.JsonFields;
+using OsmIntegrator.Tests.Fixtures;
+using OsmIntegrator.Tools;
+using Xunit;
+using System.Net.Http;
+
+namespace OsmIntegrator.Tests.Tests.GtfsImports
+{
+  public class RevertStopTest : ImportTestBase
+  {
+    public RevertStopTest(ApiWebApplicationFactory fixture) : base(fixture)
+    {
+
+    }
+
+    [Fact]
+    public async Task RevertOnlyTest()
+    {
+      await InitTest(nameof(RevertStopTest), "supervisor2", "supervisor1");
+
+      var content = new MultipartFormDataContent();
+      var fileStreamContent = new StreamContent(File.OpenRead($"{TestDataFolder}{nameof(RevertStopTest)}/Data.txt"));
+      fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
+
+      content.Add(fileStreamContent, "file", "Data.txt");
+
+      await Put_UpdateGtfsStops(content);
+
+      content = new MultipartFormDataContent();
+      fileStreamContent = new StreamContent(File.OpenRead($"{TestDataFolder}{nameof(RevertStopTest)}/Data_Reverted.txt"));
+      fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
+
+      content.Add(fileStreamContent, "file", "Data.txt");
+
+      Report report = await Put_UpdateGtfsStops(content);
+
+      string actualTxtReport = report.Value;
+      string expectedTxtReport =
+        File.ReadAllText($"{TestDataFolder}{nameof(RevertStopTest)}/Report.txt");
+
+      Assert.Equal(expectedTxtReport, actualTxtReport);
+
+      DbStop actualStop1 = _dbContext.Stops.AsNoTracking().First(x => x.StopId == GTFS_STOP_ID_3);
+      Assert.False(actualStop1.IsDeleted);
+    }
+
+    [Fact]
+    public async Task RevertAndModifyTest()
+    {
+      await InitTest(nameof(RevertStopTest), "supervisor2", "supervisor1");
+
+      var content = new MultipartFormDataContent();
+      var fileStreamContent = new StreamContent(File.OpenRead($"{TestDataFolder}{nameof(RevertStopTest)}/Data.txt"));
+      fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
+
+      content.Add(fileStreamContent, "file", "Data.txt");
+
+      await Put_UpdateGtfsStops(content);
+
+      content = new MultipartFormDataContent();
+      fileStreamContent = new StreamContent(File.OpenRead($"{TestDataFolder}{nameof(RevertStopTest)}/Data_Reverted_Modify.txt"));
+      fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
+
+      content.Add(fileStreamContent, "file", "Data_Reverted_Modify.txt");
+
+      Report report = await Put_UpdateGtfsStops(content);
+      string actualTxtReport = report.Value;
+      string expectedTxtReport =
+        File.ReadAllText($"{TestDataFolder}{nameof(RevertStopTest)}/Report_Modify.txt");
+
+      Assert.Equal(expectedTxtReport, actualTxtReport);
+
+      DbStop actualStop1 = _dbContext.Stops.AsNoTracking().First(x => x.StopId == GTFS_STOP_ID_3);
+      Assert.False(actualStop1.IsDeleted);
+    }
+  }
+}
